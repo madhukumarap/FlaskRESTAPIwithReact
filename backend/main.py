db6189d530dd4c657ee5ead43a8617e344f8b608
madhukumarap <madhukumarap07@gmail.com>
2025-02-08 00:05:17 +0530

    updates

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,14 @@ from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Recipe
 from exts import db
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db.init_app(app)
-api = Api(app,doc='/docs')
-#serializer
+migrate = Migrate(app,db)
+api = Api(app, doc='/docs')
+
+# Serializer
 recipe_model = api.model(
     'Recipe',
     {
@@ -16,32 +19,49 @@ recipe_model = api.model(
         'description': fields.String()
     }
 )
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
-        return {'message': 'hello world '}
+        return {'message': 'hello world'}
 
 @api.route('/recipes')
 class RecipeResource(Resource):
+    @api.marshal_list_with(recipe_model)
     def get(self):
-        receipes = Recipe.query.all()
-        return jsonify(receipes)
+        recipes = Recipe.query.all()
+        return [recipe.to_dict() for recipe in recipes], 200
+
+    @api.marshal_with(recipe_model)
     def post(self):
-        data = request.json
-        recipe = Recipe(title=data['title'], description=data['description'])
+        data = request.get_json()
+        recipe = Recipe(title=data.get('title'), description=data.get('description'))
         recipe.save()
-        return jsonify(recipe)
+        return recipe.to_dict(), 201  # Fixed issue
+
 @api.route('/recipes/<int:id>')
+class SingleRecipeResource(Resource):  # Renamed class to avoid conflict
+    @api.marshal_with(recipe_model)
+    def get(self, id):
+        recipe = Recipe.query.get_or_404(id)
+        return recipe.to_dict(), 200
+    @api.marshal_with(recipe_model)
+    def delete(self, id):
+        recipe = Recipe.query.get_or_404(id)
+        recipe.delete()
+        return {"message": "Recipe deleted"}, 200
+    @api.marshal_with(recipe_model)
+    def put(self, id):
+        
+        recipe = Recipe.query.get_or_404(id)
+        data = request.get_json()
+        recipe.update(data.get('title'), data.get('description'))
+        return recipe.to_dict(), 200
+
+# Shell context
 @app.shell_context_processor
-class RecipeResource(Resource):
-    def get(self,id):
-        pass
-    def delete(self,id):
-        pass
-    def put(self,id):
-        pass
-    
 def make_shell_context():
-    return {'db': db, 'Recipe': Recipe} # we dict
+    return {'db': db, 'Recipe': Recipe}
+
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,26 +1,55 @@
 from exts import db
 
-#     """
-# class Recipe:
-#     id:int primary key
-#     title:str
-#     description:str (text)        
-# """
 class Recipe(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(), nullable=False)
-    description = db.Column(db.Text(), nullable=False)
-    
-    def __repr__(self):  #the repr works as a string representation of the object
-        return f'<Recipe {self.title}>' 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)  # Set max length for efficiency
+    description = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):  
+        return f'<Recipe {self.title}>'
+
     def save(self):
         db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, title, description):
+        self.title = title
+        self.description = description
+        db.session.commit()
+
+    def to_dict(self):  
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+        }
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False,unique=True)
+    email = db.Column(db.String(), nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+    def __repr__(self):
+        return f"<User {self.username}>"
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def update(self,username,email,password):
+        self.username = username,
+        self.email = email
+        self.password = password
         db.session.commit()
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-    def update(self,title,description):
-        self.title = title
-        self.description = description
-        db.session.commit()
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "password":self.password
+        }
         
